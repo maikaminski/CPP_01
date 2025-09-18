319690f8157de3615febc8e55e1feac016c6b30e
Maira Kaminski <mahkaminski@gmail.com>
2025-09-18 18:41:31 -0300

    implementation ex00 module 01

---
+++ ex00/Zombie.cpp
@@ -0,0 +1,13 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name) : name(name) {
+    std::cout << "Zombi " << this->name << " created." << std::endl;
+}
+
+Zombie::~Zombie() {
+    std::cout << "Zombie " << this->name << " destroyed." << std::endl;
+}
+
+void Zombie::announce(void) {
+    std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
